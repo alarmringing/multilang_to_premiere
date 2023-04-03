492de63f36e34b9d93e49d4d5b8d93203e43484d
Jihee Hwang <alarmringing@gmail.com>
2023-04-03 15:26:47 -0400

    Add stats generating ability in generate_translate_captions.
    Instead of using the full google translate api, you can now use the
    --stats flag to just get the perecentage of each language through the
    detect_language api.
    If you specify both translation and --stats, it will grab the language
    percentage stats from the response of the translation api. However, I
    haven't confirmed if this works yet. The response proto format may
    differ from the detect_language api call.

--- generate_translated_captions.py
+++ generate_translated_captions.py
@@ -5,24 +5,52 @@ import argparse
 import pysrt
 import html
 from google.cloud import translate
+from google.cloud.translate_v3.types import translation_service
 from google.oauth2 import service_account
 
-def translate_captions(srt_path, google_api_key_path):
+def generate_stats(response_from_translate_api, captions):
+    language_durations = {'ko':0.0, 'en':0.0, 'zh':0.0, 'ja':0.0}
+    total_speech_duration = 0.0
+    for i in range(len(response_from_translate_api)):
+        language = response_from_translate_api[i].languages[0].language_code
+        if (language == 'zh-CN' or language == 'zh-TW'):
+            language = 'zh'
+        if language not in ['ko', 'zh', 'en', 'ja']:
+            continue
+        duration = (captions[i].end - captions[i].start).seconds + 0.001 * (captions[i].end - captions[i].start).milliseconds
+        total_speech_duration += duration
+        language_durations[language] += duration
+    print("total_speech_duration is ", total_speech_duration)
+    for lang in language_durations:
+        print("Language", lang, " percentage: ", float(language_durations[lang]) / float(total_speech_duration))
+
+def translate_captions(srt_path, google_api_key_path, args):
     
     captions = pysrt.open(srt_path)
-    
+        
     credentials = service_account.Credentials.from_service_account_file(google_api_key_path)
     client = translate.TranslationServiceClient(credentials=credentials)
-    
     project_id = credentials.project_id
     assert(project_id)
     location = 'global'
+    parent = f'projects/{project_id}/locations/{location}'
     
-    translation_target_languages = ['en']
-    
+    if (args.stats and not (args.english and args.four_languages)):
+        print("Only running language detection model.")
+        response = []
+        for caption in captions:
+            response.append(client.detect_language(content=caption.text, parent=parent))
+        generate_stats(response, captions)
+        return
+   
+    translation_target_languages = []
+    if (args.english):
+        translation_target_languages = ['en']
+    if (args.four_languages):
+        translation_target_languages = ['en', 'ja', 'ko', 'zh-CN']
+        
     for target_language in translation_target_languages:
         print("Translating " + srt_path + " to " + target_language + "...")
-        parent = f'projects/{project_id}/locations/{location}'
         response = client.translate_text(
             contents=[caption.text for caption in captions],
             target_language_code=target_language,
@@ -32,12 +60,18 @@ def translate_captions(srt_path, google_api_key_path):
         if(len(response.translations) != len(captions)):
             sys.exit("Error: length of translated results does not match the length of captions.")
         
+        if (target_language == translation_target_languages[0] and args.stats):
+            # Generate language statistics if args.stats is on.
+            generate_stats(response, captions)
+            
         translated_captions = []
         for i in range(len(response.translations)):
             text = html.unescape(response.translations[i].translated_text)
             translated_caption = pysrt.SubRipItem(captions[i].index, captions[i].start, captions[i].end, text, captions[i].position)
             translated_captions.append(translated_caption)
         
+        if target_language == 'zh-CN':
+            target_language = 'zn'
         translated_srt_outpath = os.path.join(os.path.dirname(srt_path), os.path.basename(srt_path).split('.')[0] + '_' + target_language + '.srt')
         pysrt.SubRipFile(translated_captions).save(translated_srt_outpath, encoding='utf-8')
         print("Translated captions outputted to ", translated_srt_outpath)
@@ -47,12 +81,17 @@ parser = argparse.ArgumentParser(
 parser.add_argument("srt_path", help="Path to the srt caption file to process.")
 parser.add_argument('google_api_key_path',
                     help="Path to the key to use for google translate api. Otherwise translation is not possible.")
+parser.add_argument('--english', action='store_true', help="Translate for english only.")
+parser.add_argument('--four_languages', action='store_true', help='Translate for all languages: English, Chinese, Korean, Japanese')
+parser.add_argument('--stats', action='store_true', help='Return stats for percentage of each language.')
 
 args = parser.parse_args()
 srt_path = os.path.abspath(args.srt_path)
 google_api_key_path = os.path.abspath(args.google_api_key_path)
 
 if (os.path.isfile(srt_path) and srt_path.endswith('.srt') and os.path.isfile(google_api_key_path) and google_api_key_path.endswith('.json')):
-    translate_captions(srt_path, google_api_key_path)
+    if ((args.english and args.all)):
+        sys.exit("Specify only one of the two flags: --english, or --four_languages.")
+    translate_captions(srt_path, google_api_key_path, args)
 else:
     sys.exit("Input arguments are not valid, either wrong path or file extension.")
